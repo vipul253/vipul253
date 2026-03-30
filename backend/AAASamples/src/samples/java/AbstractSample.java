b64dbe5c7374586ea95c53ea2faad6008d78c79d
vipul253 <vipulsawant25@gmail.com>
2026-03-30 10:10:57 +0530

    re-commit

---
+++ backend/AAASamples/src/samples/java/AbstractSample.java
@@ -0,0 +1,14 @@
+package samples.java;
+
+public abstract class AbstractSample {
+
+	// private won't do for abstract
+	// private abstract void method1();
+	public abstract void method1();
+
+	abstract void method2();
+
+	public void method3() {
+		// sample concrete method
+	}
+}
