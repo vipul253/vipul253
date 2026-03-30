2abbc12310626aa3ccffa72c6f395cd8149ed959
vipul253 <vipulsawant25@gmail.com>
2026-03-30 10:07:01 +0530

    more samples

--- backend/AAASamples/src/samples/java/DetailsNotFound.java
+++
@@ -1,9 +0,0 @@
-package samples.java;
-
-public class DetailsNotFound extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public DetailsNotFound(String msg) {
-		super(msg);
-	}
-}
--- backend/SpringDemo/src/main/java/com/sample/controller/GlobalExceptionHandler.java
+++ backend/SpringDemo/src/main/java/com/sample/controller/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.sample.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,10 +13,19 @@ import com.sample.exception.ResourceNotFoundException;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        Map<String, String> errorResponse = new HashMap<>();
-        errorResponse.put("error", "Not Found");
-        errorResponse.put("message", ex.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
+	public ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+		Map<String, String> errorResponse = new HashMap<>();
+		errorResponse.put("error", "Not Found");
+		errorResponse.put("message", ex.getMessage());
+		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
 }
+
+/*
+ * @RestControllerAdvice and @ControllerAdvice both annotations provide a
+ * mechanism for global exception handling and other cross-cutting concerns
+ * (like @InitBinder and @ModelAttribute) across multiple controllers, keeping
+ * your controller code clean and organized. By default, they apply to all
+ * controllers in the application, but you can narrow their scope using
+ * attributes like basePackages or annotations
+ */
