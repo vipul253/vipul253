ba6d39acb3fde79d01fdd68fdf1c63da57a4aebe
vipul253 <vipulsawant25@gmail.com>
2026-04-30 11:29:04 +0530

    test synch

---
+++ JavaScriptInterview/BOM.js
@@ -0,0 +1,14 @@
+//Browser object model window.
+//alert(), prompt(), confirm(), 
+//window.location URL (hostname,protocal,href)
+//-||- history - back forward html
+//navigator - version,name, javaEnabled(), cookieEnabled()
+//screen - height, width, colorDepth
+//document - DOM
+
+//Event Handling
+// onClick, onMouseOver, onFocus, onSelect, onBlur, onChange
+
+//DOM
+//DOM parser - converts to heirachy tree structure
+//DOM API - modify DOM structure
