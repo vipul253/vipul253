104ee15963701c877f06c3167c0b833f42ea6cde
vipul253 <vipulsawant25@gmail.com>
2026-05-06 01:03:36 +0530

    package name fix

---
+++ backend/AAASamples/src/samples/functional/Employee.java
@@ -0,0 +1,57 @@
+package samples.functional;
+
+public class Employee {
+
+	long eid;
+	String name;
+	int age;
+	double salary;
+	String designation;
+	String gender;
+	String department;
+
+	public Employee(long eid, String name, int age, double salary, String designation, String gender,
+			String department) {
+		this.eid = eid;
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+		this.designation = designation;
+		this.gender = gender;
+		this.department = department;
+	}
+
+	public long getEid() {
+		return eid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	@Override
+	public String toString() {
+		return "eid=" + eid + ", name='" + name + '\'' + ", age=" + age + ", salary=" + salary + ", designation='"
+				+ designation + '\'' + ", gender='" + gender + '\'' + ", department='" + department + '\'' + "\n";
+	}
+}
